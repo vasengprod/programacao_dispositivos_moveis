231836fd916ddd8c341cf692d482a7bcc5b28e54
Vinícius de Almeida Silva <vinicius.asilva@ufpe.br>
2026-04-21 19:04:07 +0000

    adiciona cors no backend

--- alterar-backend/backend-tarefas/app.js
+++ alterar-backend/backend-tarefas/app.js
@@ -1,12 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 let tarefas = [];
 
-// NOVA ROTA RAIZ
 app.get("/", (req, res) => {
   res.json({
     status: "ok",
